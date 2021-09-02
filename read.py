2d0078a2710f56b6bdb9f6db489eb0eb42f117b8
ajdaniel17 <ajdaniel17@yahoo.com>
2021-09-02 11:40:42 -0500

    Added Color filter on circle detection, It should now only identify orange circles

--- read.py
+++ read.py
@@ -6,8 +6,28 @@ cap = cv2.VideoCapture(0)
 
 while(True):
     ret, frame = cap.read()
-    gray = cv2.medianBlur(cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2GRAY),5)
-    circles=cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.5, 50)# ret=[[Xpos,Ypos,Radius],...]
+
+    #Convert frame to HSV values
+    hsvFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+  
+    #Define Upper and lower color limits, then define the mask
+    orange_lower = np.array([9, 50,50 ], np.uint8) #Note: I had to experiment to try and get the upper and lower limits of orange, I used https://pinetools.com/image-color-picker
+    orange_upper = np.array([50, 255, 255], np.uint8)
+    orange_mask = cv2.inRange(hsvFrame, orange_lower, orange_upper)
+
+    #I have no idea what this is for
+    kernal = np.ones((5, 5), "uint8")
+
+    #redefine the orange mask using the kernal and then combine the frame and the mask
+    orange_mask = cv2.dilate(orange_mask, kernal)
+    res_orange = cv2.bitwise_and(frame, frame, mask = orange_mask)
+
+    #split up the values of the HSV masks, V is in greyscale
+    h, s, v1 = cv2.split(res_orange)
+
+    #gray = cv2.medianBlur(cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2GRAY),5)
+    #Use v1, which only shows orange colors and is in greyscale to find circles
+    circles=cv2.HoughCircles(v1, cv2.HOUGH_GRADIENT, 1.5, 50)
     if circles is not None:
         print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")      
         circles = np.uint16(np.around(circles))
@@ -20,7 +40,7 @@ while(True):
             cv2.circle(frame, center, radius, (0, 255, 0), 3)
     else:
         print("No Circle!")
-    cv2.imshow('video',frame)
+    cv2.imshow('video',np.hstack([frame,res_orange]))
     if cv2.waitKey(1)==27:# esc Key
         break
 cap.release()
