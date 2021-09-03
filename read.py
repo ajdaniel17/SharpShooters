10de361ba1f321b6123605ade24ad5ef07af7b4d
ajdaniel17 <ajdaniel17@yahoo.com>
2021-09-02 21:34:39 -0500

    Added Servo with arduino code

--- read.py
+++ read.py
@@ -6,6 +6,7 @@ import sys
 import serial,time #Needed to communicate with Arduino
 
 cap = cv2.VideoCapture(0)
+ArduinoSerial =serial.Serial('com5',9600,timeout=0.1)
 
 while(True):
     ret, frame = cap.read()
@@ -14,8 +15,8 @@ while(True):
     hsvFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
   
     #Define Upper and lower color limits, then define the mask
-    orange_lower = np.array([9, 50,50 ], np.uint8) #Note: I had to experiment to try and get the upper and lower limits of orange, I used https://pinetools.com/image-color-picker
-    orange_upper = np.array([50, 255, 255], np.uint8)
+    orange_lower = np.array([0, 50,50 ], np.uint8) #Note: I had to experiment to try and get the upper and lower limits of orange, I used https://pinetools.com/image-color-picker
+    orange_upper = np.array([255, 250, 250], np.uint8)
     orange_mask = cv2.inRange(hsvFrame, orange_lower, orange_upper)
 
     #I have no idea what this is for
@@ -30,19 +31,22 @@ while(True):
 
     #gray = cv2.medianBlur(cv2.cvtColor(cap.read()[1], cv2.COLOR_BGR2GRAY),5)
     #Use v1, which only shows orange colors and is in greyscale to find circles
-    circles=cv2.HoughCircles(v1, cv2.HOUGH_GRADIENT, 1.5, 50)
+    circles=cv2.HoughCircles(v1, cv2.HOUGH_GRADIENT, 1.3, 50)
     if circles is not None:
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")      
+        #print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")      
         circles = np.uint16(np.around(circles))
         for i in circles[0, :]:
             center = (i[0], i[1])
+            string = 'X{0:d}'.format(i[0])
+            print(string)
+            ArduinoSerial.write(string.encode('utf-8'))
             # circle center
             cv2.circle(frame, center, 1, (0, 0, 255), 3)
             # circle outline
             radius = i[2]
             cv2.circle(frame, center, radius, (0, 255, 0), 3)
-    else:
-        print("No Circle!")
+    #else:
+       # print("No Circle!")
     cv2.imshow('video',np.hstack([frame,res_orange]))
     if cv2.waitKey(1)==27:# esc Key
         break
